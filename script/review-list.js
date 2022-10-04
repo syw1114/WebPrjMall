711370a37a07c53ba20e0529f732ffaa8c6d0bf9
syw1114 <syw1114>
2022-10-05 01:06:25 +0900

    온클릭 자바스크립트 추가

--- script/review-list.js
+++ script/review-list.js
@@ -1,40 +1,5 @@
+const reviewButton = document.querySelector("#btn-w");
 
-const writeButton = document.querySelector("#btn-w");
-
-let titleList = new Array();
-
-writeButton.onclick = () => {
-    send();
-    load();
-}
-
-function send() {
-    const inputTitle = document.querySelector("#title");
-
-    let title = {
-        title: inputTitle.value
-    }
-
-    titleList.push(title);
-}
-
-function load() {
-    const tbody = document.querySelector("#load");
-
-    tbody.innerHTML = "";
-
-    titleList.forEach((title, index) => {
-        tbody.innerHTML += `
-            <tr>
-            <td>-</td>
-            <td colspan="4">-</td>
-            <td>${title.title}</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-            </tr>
-        `
-    });
-
-    
+reviewButton.onclick = () => {
+  location.href = "/review.html";
 }
---
+++ script/review.js
@@ -0,0 +1,5 @@
+const listButton = document.querySelectorAll("#bt-color")[0];
+
+listButton.onclick = () => {
+  location.href = "/review-list.html";
+}
